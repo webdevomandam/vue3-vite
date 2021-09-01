e8316fca18738ae86d0bb54d5911a0db1fadc14a
webdevomandam <devomandam.webdev@gmail.com>
2021-09-01 19:49:51 +0800

    init commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import PrimeVue from 'primevue/config'
+
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
+const app = createApp(App)
+app.use(PrimeVue, {ripple: true})
+app.mount('#app')
